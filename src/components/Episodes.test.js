9688ce443aba591c77340548bb0611e7b08ee508
Alison Law <alisparklaw@gmail.com>
2020-02-25 15:05:16 +0000

    Episodes.test working

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -7,12 +7,30 @@ describe("Display component", () => {
   afterEach(rtl.cleanup);
 
   beforeEach(() => {
-    const episodes = [];
+  
+    const episodes = [
+{
+  id: 553946,
+url: "http://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
+name: "Chapter One: The Vanishing of Will Byers",
+season: 1,
+number: 1,
+airdate: "2016-07-15",
+airtime: "",
+airstamp: "2016-07-15T12:00:00+00:00",
+runtime: 60,
+image: {medium: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
+ original: "http://static.tvmaze.com/uploads/images/original_untouched/67/168918.jpg"},
+summary: "<p>A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.</p>",
+_links:{self: {href: "http://api.tvmaze.com/episodes/553946"}},
+}
+      
+    ];
     wrapper = rtl.render(<Episodes episodes={episodes} />);
   });
 
-  it.skip("renders without crashing", async () => {
-    const episodeDiv = await wrapper.queryByText("Season");
+  it("renders without crashing", async () => {
+    const episodeDiv = await wrapper.findByText(/episode/i);
 
     expect(episodeDiv).toBeInTheDocument();
     expect(episodeDiv).toBeVisible();
